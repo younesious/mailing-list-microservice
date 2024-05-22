891f7596d10afd528914567c27837df9d6083204
younesious <younesious80@gmail.com>
2024-05-22 13:41:37 +0330

    add handler for endpoints

---
+++ json_api/handlers.go
@@ -0,0 +1,210 @@
+package jsonapi
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/younesious/mailinglist/db"
+)
+
+func setJsonHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-type", "application/json; charset=utf8")
+}
+
+func fromJson[T any](body io.Reader, target T) error {
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(body)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(buf.Bytes(), target)
+}
+
+func toJson[T any](w http.ResponseWriter, withDate func() (T, error)) {
+	setJsonHeader(w)
+
+	data, serverErr := withDate()
+	if serverErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		serverErrJson, err := json.Marshal(map[string]string{"error": serverErr.Error()})
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Write(serverErrJson)
+		return
+	}
+
+	dataJson, err := json.Marshal(&data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Write(dataJson)
+}
+
+func returnErr(w http.ResponseWriter, err error, code int) {
+	toJson(w, func() (interface{}, error) {
+		w.WriteHeader(code)
+		return map[string]string{"error": err.Error()}, nil
+	})
+}
+
+func AddEmailHandler(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			returnErr(w, errors.New("invalid request method"), http.StatusMethodNotAllowed)
+			return
+		}
+
+		email := db.EmailEntry{}
+		if err := fromJson(r.Body, &email); err != nil {
+			returnErr(w, err, http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		id, err := db.AddEmailEntry(ctx, database, email.Email)
+		if err != nil {
+			returnErr(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		toJson(w, func() (interface{}, error) {
+			return map[string]int64{"id": id}, nil
+		})
+	}
+}
+
+func GetEmailHandler(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			returnErr(w, errors.New("invalid request method"), http.StatusMethodNotAllowed)
+			return
+		}
+
+		email := r.URL.Query().Get("email")
+		if email == "" {
+			returnErr(w, errors.New("email is required"), http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		entry, err := db.GetEmailEntry(ctx, database, email)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				returnErr(w, errors.New("email not found"), http.StatusNotFound)
+				return
+			}
+			returnErr(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		toJson(w, func() (interface{}, error) {
+			return entry, nil
+		})
+	}
+}
+
+func UpdateEmailHandler(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			returnErr(w, errors.New("invalid request method"), http.StatusMethodNotAllowed)
+			return
+		}
+
+		var entry db.EmailEntry
+		if err := fromJson(r.Body, &entry); err != nil {
+			returnErr(w, err, http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		if err := db.UpdateEmailEntry(ctx, database, entry); err != nil {
+			returnErr(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		toJson(w, func() (interface{}, error) {
+			return map[string]string{"message": "email updated successfully"}, nil
+		})
+	}
+}
+
+func DeleteEmailHandler(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			returnErr(w, errors.New("invalid request method"), http.StatusMethodNotAllowed)
+			return
+		}
+
+		email := r.URL.Query().Get("email")
+		if email == "" {
+			returnErr(w, errors.New("email is required"), http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		if err := db.DeleteEmailEntry(ctx, database, email); err != nil {
+			returnErr(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		toJson(w, func() (interface{}, error) {
+			return map[string]string{"message": "email deleted successfully"}, nil
+		})
+	}
+}
+
+func GetEmailBatchHandler(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			returnErr(w, errors.New("invalid request method"), http.StatusMethodNotAllowed)
+			return
+		}
+
+		page, err := strconv.Atoi(r.URL.Query().Get("page"))
+		if err != nil || page <= 0 {
+			page = 1
+		}
+
+		count, err := strconv.Atoi(r.URL.Query().Get("count"))
+		if err != nil || count <= 0 {
+			count = 10
+		}
+
+		params := db.GetEmailBatchQueryParam{
+			Page:  page,
+			Count: count,
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		batch, err := db.GetEmailBatch(ctx, database, params)
+		if err != nil {
+			returnErr(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		toJson(w, func() (interface{}, error) {
+			return batch, nil
+		})
+	}
+}
--- main.go
+++ main.go
@@ -1,98 +1,49 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"log"
-	"time"
+	"net/http"
+	"sync"
 
-	// Adjust the import path to your actual project structure
+	"github.com/younesious/mailinglist/db"
+	"github.com/younesious/mailinglist/jsonapi"
 
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/younesious/mailinglist/db"
 )
 
+func runServer(addr string, database *sql.DB) {
+	http.HandleFunc("/email/add", jsonapi.AddEmailHandler(database))
+	http.HandleFunc("/email/get", jsonapi.GetEmailHandler(database))
+	http.HandleFunc("/email/update", jsonapi.UpdateEmailHandler(database))
+	http.HandleFunc("/email/delete", jsonapi.DeleteEmailHandler(database))
+	http.HandleFunc("/email/batch", jsonapi.GetEmailBatchHandler(database))
+
+	log.Printf("Starting server on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	// Open the database connection
+	// we can hard code pass mailinglist.db file for this small project or read from env file for more complex project
 	database, err := sql.Open("sqlite3", "mailinglist.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer database.Close()
 
-	// Initialize the database
 	if err := db.InitializeDB(database); err != nil {
 		log.Fatal(err)
 	}
 
-	// Create a context with a timeout for the operation
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel() // Ensure resources are cleaned up
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	// Add a new email entry
-	emailID, err := db.AddEmailEntry(ctx, database, "test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Added Email Entry with ID:", emailID)
-	// Retrieve the email entry
-	emailEntry, err := db.GetEmailEntry(ctx, database, "test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Retrieved Email Entry:", emailEntry)
+	go func() {
+		defer wg.Done()
+		runServer(":8080", database)
+	}()
 
-	// Update the email entry
-	emailEntry.OptOut = true
-	err = db.UpdateEmailEntry(ctx, database, *emailEntry)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Email entry was updated successfully")
-
-	// Retrieve the email entry
-	emailEntry, err = db.GetEmailEntry(ctx, database, "test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Retrieved Email Entry:", emailEntry)
-
-	// Delete the email entry
-	err = db.DeleteEmailEntry(ctx, database, "test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Email entry deleted successfully")
-
-	// Add a new email entry
-	emailID, err = db.AddEmailEntry(ctx, database, "test4@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Added Email Entry with ID:", emailID)
-
-	// Add a new email entry
-	emailID, err = db.AddEmailEntry(ctx, database, "test2@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Added Email Entry with ID:", emailID)
-
-	// Add a new email entry
-	emailID, err = db.AddEmailEntry(ctx, database, "test3@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Added Email Entry with ID:", emailID)
-
-	// Retrieve a batch of email entries
-	params := db.GetEmailBatchQueryParam{
-		Page:  1,
-		Count: 5,
-	}
-	emailBatch, err := db.GetEmailBatch(ctx, database, params)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Retrieved Email Batch:", emailBatch)
+	wg.Wait()
 }
